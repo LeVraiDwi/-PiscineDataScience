14abbf62f6661c4b04f0e20dee8b15139cc2a666
Theo COSSE <tcosse@paul-f4Ar2s1.clusters.42paris.fr>
2024-07-17 19:42:43 +0200

    customJupImage

--- docker/volumes/datable/create_db.sh
+++ docker/volumes/datable/create_db.sh
@@ -3,3 +3,4 @@ psql -U tcosse -h localhost -d piscineds -w -f /home/automatic_table.sql
 psql -U tcosse -h localhost -d piscineds -w -f /home/items_table.sql
 psql -U tcosse -h localhost -d piscineds -w -f /home/customers_table.sql
 psql -U tcosse -h localhost -d piscineds -w -f /home/remove_duplicates.sql
+psql -U tcosse -h localhost -d piscineds -w -f /home/fusion.sql
