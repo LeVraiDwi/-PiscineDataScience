54a2220d9027907f15c4ec724887a2ec2d87296f
Theo COSSE <tcosse@paul-f4Ar7s11.clusters.42paris.fr>
2024-07-18 21:56:16 +0200

    dsd

--- docker/volumes/datable/remove_duplicates.sql
+++ docker/volumes/datable/remove_duplicates.sql
@@ -5,11 +5,14 @@ begin
     create TABLE tmp as with remove_duplicates as (
         SELECT *, LAG(event_time) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevTime,
         LAG(product_id) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevProduct,
-        LAG(event_type) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevEvent_type
+        LAG(event_type) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevEvent_type,
+        LAG(user_id) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevUser_id,
+        LAG(user_session) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevUser_session,
+        LAG(price) over (partition by event_type, product_id, price, user_id, user_session ORDER BY event_time) as prevPrice
         FROM customers)
     SELECT event_time, event_type, product_id, price, user_id, user_session
     FROM remove_duplicates
-    where prevEvent_type != event_type or prevProduct != product_id or prevTime IS NULL or event_time - prevTime > INTERVAL '1 second';
+    where user_id != user_id or user_session != user_session or price != price or prevEvent_type != event_type or prevProduct != product_id or event_time - prevTime > INTERVAL '1 second' or prevTime IS NULL;
 
     DROP TABLE customers;
     alter TABLE tmp RENAME TO customers;
